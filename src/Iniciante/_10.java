57189f11491257571f749e50c47d1a0ad37c8268
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-04-14 14:20:17 -0300

    Ajuste desafio 10.

--- src/Iniciante/_10.java
+++ src/Iniciante/_10.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
 public class _10 {
     //10-Crie um programa para inverter uma string.
 
+    static ArrayList <Character> inv = new ArrayList<>();
+    public static void inverter(String word){
+        for(int i =0;i<word.length();i++){
+            inv.add(word.charAt(i));
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -13,10 +20,7 @@ public class _10 {
         System.out.println("Type a word:");
         String word = sc.nextLine();
 
-        ArrayList <Character> inv = new ArrayList<>();
-        for(int i =0;i<word.length();i++){
-            inv.add(word.charAt(i));
-        }
+        inverter(word);
 
         for(int i =inv.size()-1;i>=0;i--){
                 System.out.print(inv.get(i));
